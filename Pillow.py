5ebbcaa9550048f52c3f277c7e4e249da6c4a047
Yous113 <44925411+Yous113@users.noreply.github.com>
2023-03-13 12:27:09 +0100

    pillow

--- Pillow.py
+++ Pillow.py
@@ -1,10 +1,17 @@
 import discord
 from discord.ext import commands
 from PIL import Image, ImageDraw, ImageFont
+from MemeGeneratorBot import client
 
-async def draw(ctx):
-    img = Image.open('memepic.png')
-    draw = ImageDraw.Draw(img)
-    draw.text((0,0), 'lorum ipsum', fill=(255, 255, 255))
-    img = img.save('output.png')
-    display(img)
+@client.command()
+async def meme(ctx, user:discord.Member = None):
+    if user == None:
+        user = ctx.author
+    meme = Image.open('memepic.png')
+    textup_font = ImageFont.truetype('impact/impact.ttf', 200)
+    textup_text = 'meme text'
+    textdown_font = ImageFont.truetype('impact/impact.ttf', 200)
+    textdown_text = 'memeunder text'
+    image_editable = ImageDraw.Draw(meme)
+    image_editable.text((15,15), textup_text, (252, 252, 252), font=textup_font)
+    image_editable.text((215,215), textdown_text, (252, 252, 252), font=textdown_font)
