41ebbc6db905e7058bf334b4bd0956c0a2411143
Yous113 <44925411+Yous113@users.noreply.github.com>
2023-04-06 18:06:53 +0200

    Print my memes + debugging
    Print my memes funktion virker nu. Fandt en fejl, som ikke gav lov til at lave en ny meme efter du har lavet din første. den er ny fixet ved at clear key value pair når du laver en meme, samt en indentation fejl.

--- MemeGeneratorBot.py
+++ MemeGeneratorBot.py
@@ -45,33 +45,33 @@ async def on_message(message):
       for template, file in Storage.templates:
         await message.channel.send(template)
     
-    if contents.startswith("!"):
+    """ if contents.startswith("!"):
       con = contents[1:]
       for key in dict.keys(Storage.templates):
         key == con
         for con in Storage.templates:
-          await message.channel.send(dict.values(Storage.templates))
+          await message.channel.send(dict.values(Storage.templates)) """
 
     if contents.startswith("!image"):
       Storage.memedict[user] = Storage.meme(user)
-      Storage.memedict[user].Printvalues()
       if len(Storage.memeStorage) <= 0:
         if user not in Storage.memeStorage:
           Storage.memeStorage[user] = []
-        if len(attachments) <= 0:
-          await message.channel.send("Attach a picture after '!image' and then type 'text'")
+      if len(attachments) <= 0:
+        await message.channel.send("Attach a picture after '!image' and then type 'text'")
+      else:
+        picture = attachments[0]
+        filename = picture.filename
+        print(filename)
+      # check if the file that is attached is an image(we use funktion lower() to make sure that like PNG also gets through)
+        if filename.lower().endswith((".jpg", ".png", ".jpeg")):
+          Storage.memedict[user].image_data = await picture.read()
+          Storage.memedict[user].image = Image.open(io.BytesIO(Storage.memedict[user].image_data))
+          Storage.memedict[user].Printvalues()
+          print("got the picture")
+          await message.channel.send("Send the upper text to the meme. following '!textup'.")
         else:
-          picture = attachments[0]
-          filename = picture.filename
-          print(filename)
-        # check if the file that is attached is an image(we use funktion lower() to make sure that like PNG also gets through)
-          if filename.lower().endswith((".jpg", ".png", ".jpeg")):
-            Storage.memedict[user].image_data = await picture.read()
-            Storage.memedict[user].image = Image.open(io.BytesIO(Storage.memedict[user].image_data))
-            print("got the picture")
-            await message.channel.send("Send the upper text to the meme. following '!textup'.")
-          else:
-            await message.channel.send("Attach a picture after '!image'")
+          await message.channel.send("Attach a picture after '!image'")
 
     if contents.startswith("!textup"):
       if user in Storage.memedict:
@@ -106,6 +106,8 @@ async def on_message(message):
       if user in Storage.memedict:
         if Storage.memedict[user].i == 2:
           await Pillow.meme(message, user)
+          del Storage.memedict[user]
+          print(Storage.memedict)
           print(Storage.memeStorage)
           # with io.BytesIO() as image_binary:
           #   Storage.memedict[user].image.save(image_binary, 'PNG')
@@ -121,23 +123,8 @@ async def on_message(message):
         await message.channel.send(Storage.defaultReply)
     
     if contents.startswith("!Print my memes"):
-      if len(Storage.memeStorage) <= 0:
-        if user in Storage.memeStorage:
-          for user, list in Storage.memeStorage:
-            for meme in list:
-              with io.BytesIO() as image_binary:
-                meme.save(image_binary, 'PNG')
-                image_binary.seek(0)
-                file = discord.File(fp=image_binary, filename='image.png')
-                await message.channel.send(file=file)
-        else:
-          await message.channel.send("You have no memes, use command '!make a meme' to make your first meme")      
-      else:
-        await message.channel.send("You have no memes, use command '!make a meme' to make your first meme")      
+      await Pillow.send_user_memes(message, user)
                
-
-
-
 token = get_token()
 client.run(token)
 
--- Pillow.py
+++ Pillow.py
@@ -5,27 +5,55 @@ from discord.ext import commands
 from PIL import Image, ImageDraw, ImageFont
 import Storage
 
-async def meme(ctx, user:discord.Member = None):
-    if user == None:
+async def meme(ctx, user: discord.Member = None):
+    if user is None:
         user = ctx.author
-    print("hej")
+
     user_meme = Storage.memedict[user]
     meme = Image.open(io.BytesIO(user_meme.image_data))
-    Storage.memedict[user].imagedata = io.BytesIO(user_meme.image_data)
-    #meme = user_meme
-    meme = meme.resize((250,150))
+    Storage.memedict[user].image_data = io.BytesIO(user_meme.image_data)
+    meme = meme.resize((250, 150))
+
     text_font = ImageFont.truetype('impact/impact.ttf', 25)
     textup_text = user_meme.textup
     textdown_text = user_meme.textdown
     image_editable = ImageDraw.Draw(meme)
+
     _, _, w1, _ = image_editable.textbbox((0, 0), textup_text, font=text_font)
     _, _, w2, _ = image_editable.textbbox((0, 0), textdown_text, font=text_font)
-    image_editable.text(((250-w1)/2,5), textup_text, (250, 250, 250), font=text_font)
-    image_editable.text(((250-w2)/2,115), textdown_text, (250, 250, 250), font=text_font)
-    meme.save("meme.png")
-    meme.close()
 
-    Storage.memedict[user].i += 1 
+    image_editable.text(((250 - w1) / 2, 5), textup_text, (250, 250, 250), font=text_font)
+    image_editable.text(((250 - w2) / 2, 115), textdown_text, (250, 250, 250), font=text_font)
+
+    image_binary = io.BytesIO()
+    meme.save(image_binary, format='PNG')
+    image_binary.seek(0)
+
+    # Add the meme to the user's meme storage list
+    if user not in Storage.memeStorage:
+        Storage.memeStorage[user] = []
+
+    Storage.memeStorage[user].append(image_binary)
     Storage.memedict[user].image = Image.open("meme.png")
-    Storage.memeStorage[user].append(Storage.memedict[user].image)
-    await ctx.channel.send(file = discord.File("meme.png"))
+    Storage.memedict[user].i = 0
+
+    # Send the meme as a file attachment
+    await ctx.channel.send(file=discord.File(image_binary, filename='meme.png'))
+
+async def send_user_memes(ctx, user: discord.member = None):
+    if user is None:
+        user = ctx.author
+    
+    # Check if user has memes
+    if user not in Storage.memeStorage:
+        await ctx.channel.send("You have no memes")
+        return
+    
+    # Send users memes
+    for meme in Storage.memeStorage[user]:
+        image_bytes = io.BytesIO()
+        with Image.open(meme) as img:
+            img.save(image_bytes, format='PNG')
+            image_bytes.seek(0)
+            await ctx.channel.send(file=discord.File(fp=image_bytes, filename='meme.png'))
+
